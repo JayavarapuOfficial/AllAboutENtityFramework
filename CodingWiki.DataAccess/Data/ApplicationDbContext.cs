f791dd5878389b6bb63e2c5d1e760a8690153064
JayavarapuOfficial <156125204+JayavarapuOfficial@users.noreply.github.com>
2024-01-12 12:22:26 +0530

    adding ApplicationDb context and adding book table to Db

---
+++ CodingWiki.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CodingWiki.Models.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodingWiki.DataAccess.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost\\sqlexpress;database=codingwiki;TrustServerCertificate=true;Trusted_Connection=true;");
+        }
+    }
+}
